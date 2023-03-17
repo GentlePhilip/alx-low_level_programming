eaad04bc559ba548a1af30985f55f84087312daa
GentlePhilip <aphilipnyarko@gmail.com>
2023-03-17 16:41:26 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
 #include<stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
- */
  * Description: print 0, 1, - 9
  *
  * Return: Always 0 (Success)
@@ -13,18 +10,6 @@
 
 int main(void)
 {
-    int a;
-    for (a = 0; a < 10; a++)
-    {
-        putchar(a + '0');
-        if (a < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
-    return (0);
 	int digit = 0;
 
 	while (digit <= 9)
