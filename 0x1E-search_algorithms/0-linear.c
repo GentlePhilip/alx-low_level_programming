592b31349e09804e329aebdd85e51cac0b87d4a5
GentlePhilip <aphilipnyarko@gmail.com>
2023-12-12 08:12:54 -0800

    C - Search Algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,9 +1,11 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Looks for the value in an array integers using the Linear search algorithm
- * @array : array's input
- * @size: the array size
+ * linear_search - searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
  * @value: value to search in
  * Return: Always EXIT_SUCCESS
  */
